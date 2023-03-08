5bc48410c185624dd0e4eaf34fd543a7258f0263
be <jacobhboy66@gmail.com>
2023-03-08 16:30:36 +0900

    feat: Application을 통한 게임 플레이 구현

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import baseball.controller.BaseballController;
+
+public class Application {
+    public static void main(String[] args) {
+        BaseballController.play();
+    }
+}
