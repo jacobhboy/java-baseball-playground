1396b4fd239b551fee0ecdc7300554317fce52a2
be <jacobhboy66@gmail.com>
2023-03-14 08:18:45 +0900

    feat: 게임 진행에서 필요한 문자열 출력 로직

---
+++ src/main/java/view/output/MessageOutput.java
@@ -0,0 +1,19 @@
+package view.output;
+
+public class MessageOutput {
+    private static final String GET_NUMBER = "숫자를 입력해 주세요 : ";
+    private static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다.";
+    private static final String ASK_REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void getNumber(){
+        System.out.print(GET_NUMBER);
+    }
+
+    public static void tellGameEnd(){
+        System.out.println(END_GAME);
+    }
+
+    public static void askRegame(){
+        System.out.println(ASK_REGAME);
+    }
+}
