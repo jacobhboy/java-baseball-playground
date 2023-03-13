399563bf50eed19cdaf0afb001d84006a041b152
be <jacobhboy66@gmail.com>
2023-03-13 23:52:20 +0900

    test: 수 검증 로직 테스트 추가

---
+++ src/test/java/study/BallsValidationTest.java
@@ -0,0 +1,34 @@
+package study;
+
+import model.ball.Balls;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import view.input.ValidateNumber;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BallsValidationTest {
+
+    @Test
+    void ballsValidationTest(){
+        //given
+        final int number1 = 100;
+        final int number2 = 1000;
+        final int number3 = 200;
+        final int number4 = 919;
+        final int number5 = 122;
+        //when, then
+        assertAll(
+                () -> assertThatThrownBy(
+                        () -> ValidateNumber.validate(number1)).isInstanceOf(Exception.class),
+                () -> assertThatThrownBy(
+                        () -> ValidateNumber.validate(number2)).isInstanceOf(Exception.class),
+                () -> assertThatThrownBy(
+                        () -> ValidateNumber.validate(number3)).isInstanceOf(Exception.class),
+                () -> assertThatThrownBy(
+                        () -> ValidateNumber.validate(number4)).isInstanceOf(Exception.class),
+                () -> assertThatThrownBy(
+                        () -> ValidateNumber.validate(number5)).isInstanceOf(Exception.class));
+    }
+}
--- src/test/java/study/RandomBallsTest.java
+++ src/test/java/study/RandomBallsTest.java
@@ -1,11 +1,16 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import view.input.RandomNumber;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RandomBallsTest {
     @Test
+    @DisplayName("랜덤 공 생성이 잘 되는지 테스트")
     void createRandomBall(){
         //given
         int randomNumber1 = RandomNumber.getRandomNumber();
@@ -18,14 +23,15 @@ public class RandomBallsTest {
         int randomNumber8 = RandomNumber.getRandomNumber();
 
         //when, then
-        Assertions.assertThat(randomNumber1).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber2).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber3).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber4).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber5).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber6).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber7).isBetween(100, 1000);
-        Assertions.assertThat(randomNumber8).isBetween(100, 1000);
+        assertAll(()->assertThat(randomNumber1).isBetween(100, 1000),
+                () -> assertThat(randomNumber2).isBetween(100, 1000),
+                () -> assertThat(randomNumber3).isBetween(100, 1000),
+                () -> assertThat(randomNumber4).isBetween(100, 1000),
+                () -> assertThat(randomNumber5).isBetween(100, 1000),
+                () -> assertThat(randomNumber6).isBetween(100, 1000),
+                () -> assertThat(randomNumber7).isBetween(100, 1000),
+                () -> assertThat(randomNumber8).isBetween(100, 1000)
+                );
     }
 
 }
