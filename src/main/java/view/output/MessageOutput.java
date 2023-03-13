8f88925f9f73a6f1a3229263785884f6cc5db387
be <jacobhboy66@gmail.com>
2023-03-14 08:28:40 +0900

    refactor: GET_NUMBER ASK_NUMBER로 이름변경

--- src/main/java/view/output/MessageOutput.java
+++ src/main/java/view/output/MessageOutput.java
@@ -1,12 +1,12 @@
 package view.output;
 
 public class MessageOutput {
-    private static final String GET_NUMBER = "숫자를 입력해 주세요 : ";
+    private static final String ASK_NUMBER = "숫자를 입력해 주세요 : ";
     private static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다.";
     private static final String ASK_REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public static void getNumber(){
-        System.out.print(GET_NUMBER);
+    public static void askNumber(){
+        System.out.print(ASK_NUMBER);
     }
 
     public static void tellGameEnd(){
