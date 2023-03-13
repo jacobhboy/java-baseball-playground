1e9457ca45c24405f59c32545945a680e771139b
be <jacobhboy66@gmail.com>
2023-03-14 08:34:02 +0900

    update: userNumber에 게임 추가 진행할 것인지 물어보는 메서드 추가

--- src/main/java/view/input/UserNumber.java
+++ src/main/java/view/input/UserNumber.java
@@ -3,13 +3,17 @@ package view.input;
 import java.util.Scanner;
 
 public class UserNumber {
-    public static int getUserNumber(){
-        Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static int getUserNumber(){
         int userNumber = scanner.nextInt();
 
         ValidateNumber.validate(userNumber);
 
         return userNumber;
+    }
+
+    public static int getRegameNumber(){
+        return scanner.nextInt();
     }
 }
